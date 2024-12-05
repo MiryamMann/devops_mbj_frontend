596b8d0726b3e826ae77f3f65c9acbc42aa0073d
MiriMann <m0583230707@gmail.com>
2024-12-06 01:30:07 +0200

    Automated commit message

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   const fetchData = () => {
-    fetch('http://localhost:5000/api')
+    fetch('http://backend-mig-static-ip:5000/api')
       .then(response => response.json())
       .then(data => setMessage(data.message))
       .catch(error => console.error('Error fetching data:', error));
@@ -16,7 +16,7 @@ function App() {
       <header className="App-header">
         <h1>Welcome to The Miri's App!!!</h1>
         <h1>Hashem please help me!!!!</h1>
-        <button /*onClick={fetchData}*/ className="fetch-button">Fetch backend</button>
+        <button onClick={fetchData} className="fetch-button">Fetch backend</button>
         <p>{message}</p>
       </header>
     </div>
