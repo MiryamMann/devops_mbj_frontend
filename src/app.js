5a6229f240c8e6ea588894431839472b65c35c77
MiriMann <m0583230707@gmail.com>
2024-12-06 01:48:37 +0200

    Automated commit message

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ function App() {
   };
 
   return (
+    <>
     <div className="App">
       <header className="App-header">
         <h1>Welcome to The Miri's App!!!</h1>
@@ -20,6 +21,7 @@ function App() {
         <p>{message}</p>
       </header>
     </div>
+    </>
   );
 }
 
